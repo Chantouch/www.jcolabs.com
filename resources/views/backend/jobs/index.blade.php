9a9e5f246e7c5340fd9bc23912a3b34116f2b6cc
Chantouch <Ch@n!0uch>
2016-12-13 21:35:48 +0700

    -test

--- resources/views/backend/jobs/index.blade.php
+++ resources/views/backend/jobs/index.blade.php
@@ -9,9 +9,7 @@
     </section>
     <div class="content">
         <div class="clearfix"></div>
-
         @include('flash::message')
-
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
